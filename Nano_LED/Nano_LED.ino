4431d93a56d1457d958108985aee39bdd809ae79
Fortuz <fortuz19@gmail.com>
2025-08-05 16:07:42 +0200

    Communication clearup, works with GET

--- Nano_LED/Nano_LED.ino
+++ Nano_LED/Nano_LED.ino
@@ -7,14 +7,13 @@
 CRGB leds[NUM_LEDS];
 
 int led_settings[8] = {MODE_WAVE_RIGHT, CYAN_GP, MODE_WAVE_LEFT, CYAN_GP, MODE_PULSE, PINK_GP, MODE_PULSE, PINK_GP};
-int led_settings_size = 8;
+int LED_SETTINGS_COUNT = 8;
 int new_sets[8]; 
 String output = "";
 
 void setup() {
   Serial.begin(9600);
   delay(3000); // 3 second delay for recovery
-  Serial.println("LED is ready!");
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS)
     //.setCorrection(TypicalLEDStrip) // cpt-city palettes have different color balance
@@ -26,39 +25,52 @@ void setup() {
 
 void loop()
 { 
+  output = "";
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
-  
-    for (int i=0;i<8;i++){
-      new_sets[i] = input.substring(i*2, i*2+2).toInt();
-    }
-
     
-    for (int i=0;i<8;i++){
-      if (new_sets[i] < gGradientPaletteCount){
-        led_settings[i] = new_sets[i];
-      }
+    if (input.startsWith("GET")) {
+      String output = generateLedSettingsString();
+      Serial.println(output);
+    } else {
+      parseInputToNewSettings(input);
     } 
-    
+
+    applyNewSettings();
   }
-  output = "";
   
+  // Set Led modes and colors
   setLedBatch(leds, INDEX_FL, led_settings[0], led_settings[1]);
   setLedBatch(leds, INDEX_FR, led_settings[2], led_settings[3]);
   setLedBatch(leds, INDEX_BR, led_settings[4], led_settings[5]);
   setLedBatch(leds, INDEX_BL, led_settings[6], led_settings[7]);
 
-  for(int i = 0; i < led_settings_size; i++) {
-    if (led_settings[i] < 10) {
-      output += "0";  // Add leading zero for single-digit numbers
-    }
-    output += String(led_settings[i]);
-  }
-
-  Serial.println(output);
-
   FastLED.show();
   FastLED.delay(20);
-
 }
 
+
+// Functions
+
+String generateLedSettingsString() {
+  String output = "";
+  for (uint8_t i = 0; i < LED_SETTINGS_COUNT; i++) {
+    if (led_settings[i] < 10) output += "0";
+    output += String(led_settings[i]);
+  }
+  return output;
+}
+
+void parseInputToNewSettings(const String& input) {
+  for (uint8_t i = 0; i < LED_SETTINGS_COUNT; i++) {
+    new_sets[i] = input.substring(i * 2, i * 2 + 2).toInt();
+  }
+}
+
+void applyNewSettings() {
+  for (uint8_t i = 0; i < LED_SETTINGS_COUNT; i++) {
+    if (new_sets[i] < gGradientPaletteCount) {
+      led_settings[i] = new_sets[i];
+    }
+  }
+}
